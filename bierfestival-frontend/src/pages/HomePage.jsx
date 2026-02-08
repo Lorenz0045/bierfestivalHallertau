cd901fe2f1603f445d41c60a520c76d51d4b8709
Lorenz0045 <lorenz.schermbach@outlook.de>
2026-02-08 21:42:47 +0100

    satellit-option

--- bierfestival-frontend/src/pages/HomePage.jsx
+++ bierfestival-frontend/src/pages/HomePage.jsx
@@ -1,18 +1,18 @@
 import React from 'react';
-import { MapContainer, TileLayer, Polygon, Popup } from 'react-leaflet';
+import { MapContainer, TileLayer, Polygon, Popup, LayersControl } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css'; // WICHTIG: Sonst ist die Karte kaputt (Kacheln durcheinander)
 import styles from './HomePage.module.css';
 
 const HomePage = () => {
   // Start-Koordinaten 
-    const centerPosition = [48.1351, 11.5495]; // Latitude, Longitude
+    const centerPosition = [48.50555005218888, 11.75895927999904]; // Latitude, Longitude
 
     // Ein einfaches Viereck (Polygon), das eine "Area" markiert
     const stageArea = [
-        [48.1351, 11.5495],
-        [48.1360, 11.5495],
-        [48.1360, 11.5510],
-        [48.1351, 11.5510],
+        [48.50555, 11.75896],  // Startpunkt (dein Zentrum)
+        [48.50600, 11.75896],  // Etwas nach Norden
+        [48.50600, 11.76000],  // Etwas nach Osten
+        [48.50555, 11.76000],  // Zurück nach Süden
     ];
 
     const stageStyle = { color: 'purple', fillColor: 'purple', fillOpacity: 0.4 };
@@ -21,23 +21,37 @@ const HomePage = () => {
         <div className={styles.mapWrapper}>
             <MapContainer 
                 center={centerPosition} 
-                zoom={15} 
+                zoom={20} 
                 scrollWheelZoom={true} 
                 className={styles.mapContainer}
             >
-                {/* 1. Die Basiskarte (OpenStreetMap - kostenlos) */}
-                <TileLayer
-                    attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-                    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-                />
+                <LayersControl position="topright">
+                    
+                    {/* Option 1: Standard Straßenkarte (Standard ausgewählt) */}
+                    <LayersControl.BaseLayer checked name="Straßenkarte">
+                        <TileLayer
+                            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                        />
+                    </LayersControl.BaseLayer>
 
-                {/* 2. Test-Objekt: Ein lila Bereich (klickbar!) */}
+                    {/* Option 2: Satellit (Esri World Imagery) */}
+                    <LayersControl.BaseLayer name="Satellit">
+                        <TileLayer
+                            attribution='Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community'
+                            url="https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}"
+                        />
+                    </LayersControl.BaseLayer>
+
+                </LayersControl>
+
+                {/* Das lila Test-Zelt */}
                 <Polygon pathOptions={stageStyle} positions={stageArea}>
                     <Popup>
                         <div style={{ textAlign: 'center' }}>
-                            <strong>🍻 Festzelt "Zur Post"</strong><br/>
-                            Hier gibt es Helles und Brezn.<br/>
-                            <em style={{fontSize: '0.8rem'}}>Klick für Details...</em>
+                            <strong>📍 Dein Standort-Test</strong><br/>
+                            Koordinaten: 48.505, 11.759<br/>
+                            <em style={{fontSize: '0.8rem'}}>Funktioniert!</em>
                         </div>
                     </Popup>
                 </Polygon>
